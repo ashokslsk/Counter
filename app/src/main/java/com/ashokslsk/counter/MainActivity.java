a4e1e6645c64aa47d7565049eebe7674f346c3c7
ashok.kumar <ashok.kumar@dunstconsulting.in>
2017-01-11 21:54:11 +0530

    Improved counter app

--- app/src/main/java/com/ashokslsk/counter/MainActivity.java
+++ app/src/main/java/com/ashokslsk/counter/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mText;
     private Button mButton;
     int counter = 0;
-
+    private EditText mEditText;
+    private String mUsername = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mText = (TextView) findViewById(R.id.mJapa);
         mButton = (Button) findViewById(R.id.japaClick);
+        mEditText = (EditText) findViewById(R.id.name);
+
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                counter++;
-                mText.setText(counter+"");
+                mUsername=mEditText.getText().toString();
+                if(mUsername.isEmpty()){
+
+                    Toast.makeText(MainActivity.this, "Please enter your name`", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    counter++;
+                    mText.setText(mUsername+" "+"Clicking "+counter+" Many times");
+                }
+
+
 
             }
         });
